71dc4491889a32ea1dea75ef34b419691526032c
krishna pandey <krishna7376285419@gmail.com>
2026-07-24 10:28:32 +0530

    regisration

---
+++ Day 0/index.js
@@ -0,0 +1,25 @@
+
+
+
+// hoisting
+// console.log(b);
+// let b;
+// b=3;
+// {
+//   let b="3";
+//   b=3+"3";
+//   console.log(b);
+// }
+// console.log(b);
+
+
+
+// greet();
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+var a=2;
+ a='abc';
+console.log(a);
---
+++ Day 0/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log(a);
+// let a=2;
+// console.log(typeof(a));
+// // console.log(a);
+// // let a="abc";
+// // console.log(typeof(a));
+// // console.log(object)
